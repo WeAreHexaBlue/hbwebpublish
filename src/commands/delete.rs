031a93379f6718a64a25c8e878d5a45fb12b92b1
ActuallyFlamey <flamey@hexa.blue>
2026-09-02 01:23:23 +0200

    feat: delete command

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -1,3 +1,33 @@
+use std::env;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct NewsPubDELETERes {
+    slug: String,
+    locale: String,
+}
+
 pub fn delete(slug: String, locale: String, token: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let use_test = env::var("USE_TEST")
+        .ok()
+        .and_then(|value| value.trim().parse::<bool>().ok())
+        .unwrap_or(false);
+
+    let endpoint = if !use_test { "https://hexa.blue/news/pub" } else { "http://localhost:5173/news/pub" };
+
+    let client = reqwest::blocking::Client::new();
+    let res = client
+        .delete(endpoint)
+        .bearer_auth(token)
+        .query(&[("slug", slug), ("locale", locale)])
+        .send()?;
+
+    if !res.status().is_success() {
+        return Err(format!("HTTP Error {}: {}", res.status(), res.text().unwrap_or_default()).into());
+    }
+
+    let rdata: NewsPubDELETERes = res.json()?;
+    println!("Deleted article `{}` for locale `{}`", rdata.slug, rdata.locale);
+
     Ok(())
 }
